5e9cbeeca49dac736651c9a3ccf36fc3af7f4532
Moydis <94366260+Moydis@users.noreply.github.com>
2023-01-12 13:30:34 +0100

    intial commit

---
+++ Assets/Scrips/Menu/Main Menu/Codes/THE ONE PIECE/OpenURL.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class OpenURL : MonoBehaviour
+{
+    public void Openurl()
+    {
+        Application.OpenURL("https://www.youtube.com/watch?v=dQw4w9WgXcQ");
+    }
+
+}
+
