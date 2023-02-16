344c1a7d25b36cbfb7dd1665bbaecfa796d74a0a
Moydis <94366260+Moydis@users.noreply.github.com>
2023-02-16 14:19:41 +0100

    initial commit

--- Assets/Scrips/Player/Controlls/LvL6-10/LookingController.cs
+++ Assets/Scrips/Player/Controlls/LvL6-10/LookingController.cs
@@ -6,37 +6,38 @@ using UnityEngine.InputSystem;
 public class LookingController : MonoBehaviour
 {
 
-    public float Sensititvity = 100f;
+    public float sensitivity = 5.0f;
+
 
     public Transform playerBody;
 
     float xRotation = 0f;
-   
+
+
 
 
 
     void Update()
     {
 
- 
+
 
 
         // Kontroller (kommer snart)
-         float rightstickX = Input.GetAxis("Right Joystick X") * Sensititvity * Time.deltaTime;
-         float rightstickY = Input.GetAxis("Right Joystick Y") * Sensititvity * Time.deltaTime;
-    
-         xRotation += rightstickY;
-         
+        float rightstickX = Input.GetAxis("Right Joystick X") * sensitivity * Time.deltaTime;
+        float rightstickY = Input.GetAxis("Right Joystick Y") * sensitivity * Time.deltaTime;
 
-         transform.localRotation = Quaternion.Euler(xRotation, -90f, 0f);
-         playerBody.Rotate(Vector3.up * rightstickX); 
+        xRotation += rightstickY;
 
 
+        transform.localRotation = Quaternion.Euler(xRotation, -90f, 0f);
+        playerBody.Rotate(Vector3.up * rightstickX);
+        
 
 
 
+       
     }
-
 
 }
 
